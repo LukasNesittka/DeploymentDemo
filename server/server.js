c59c51e14f18e46628e7616b72c0f3ffba7d7751
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-02-09 11:58:46 -0600

    addedPath

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
+const port = process.env.PORT || 4000
 
-app.listen(4000, () => console.log('Server running on 4000'))
+app.listen(port, () => console.log('Server running on 4000'))
